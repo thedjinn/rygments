fac94882f4f7dde546bae87f65e7a95d33055c04
Emil Loer <emil@koffietijd.net>
2011-03-01 23:47:59 +0100

    add specs

--- spec/rygments_spec.rb
+++ spec/rygments_spec.rb
@@ -1,7 +1,87 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-describe "rygments" do
-  it "should have tests" do
-    pending "test me"
+describe Rygments do
+  describe "#highlight_string" do
+    it "should return a string" do
+      Rygments.highlight_string("puts 'Hello World!'", "ruby", "html").should be_a(String)
+    end
+
+    it "should raise an exception when given an invalid formatter" do
+      expect {
+        Rygments.highlight_string("puts 'Hello World!", "ruby", "awesome_nonexistant_formatter")
+      }.to raise_error
+    end
+
+    it "should raise an exception when given an invalid lexer" do
+      expect {
+        Rygments.highlight_string("puts 'Hello World!", "awesome_nonexistant_lexer", "html")
+      }.to raise_error
+    end
+  end
+  
+  describe "#highlight_file" do
+    it "should return a string" do
+      Rygments.highlight_file(__FILE__, "ruby", "html").should be_a(String)
+    end
+
+    it "should raise an exception when given an invalid formatter" do
+      expect {
+        Rygments.highlight_file(__FILE__, "ruby", "awesome_nonexistant_formatter")
+      }.to raise_error
+    end
+
+    it "should raise an exception when given an invalid lexer" do
+      expect {
+        Rygments.highlight_file(__FILE__, "awesome_nonexistant_lexer", "html")
+      }.to raise_error
+    end
+  end
+end
+
+describe Rygments::Wrapper do
+  it "should raise an exception when given an invalid helper path" do
+    expect {
+      Rygments::Wrapper.new("this_path_does_not_exist")
+    }.to raise_error
+  end
+
+  subject do
+    Rygments::Wrapper.new(Rygments::HELPER_PATH)
+  end
+
+  describe "#highlight_string" do
+    it "should return a string" do
+      subject.highlight_string("puts 'Hello World!'", "ruby", "html").should be_a(String)
+    end
+
+    it "should raise an exception when given an invalid formatter" do
+      expect {
+        subject.highlight_string("puts 'Hello World!", "ruby", "awesome_nonexistant_formatter")
+      }.to raise_error
+    end
+
+    it "should raise an exception when given an invalid lexer" do
+      expect {
+        subject.highlight_string("puts 'Hello World!", "awesome_nonexistant_lexer", "html")
+      }.to raise_error
+    end
+  end
+
+  describe "#highlight_file" do
+    it "should return a string" do
+      subject.highlight_file(__FILE__, "ruby", "html").should be_a(String)
+    end
+
+    it "should raise an exception when given an invalid formatter" do
+      expect {
+        subject.highlight_file(__FILE__, "ruby", "awesome_nonexistant_formatter")
+      }.to raise_error
+    end
+
+    it "should raise an exception when given an invalid lexer" do
+      expect {
+        subject.highlight_file(__FILE__, "awesome_nonexistant_lexer", "html")
+      }.to raise_error
+    end
   end
 end
