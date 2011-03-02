a28aca41b47856e529c3accab7b7ac38bb0cf9fd
Emil Loer <emil@koffietijd.net>
2011-03-02 23:16:03 +0100

    update wrapper load path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 SPEC_DIR = File.dirname(__FILE__)
 lib_path = File.expand_path(File.join(SPEC_DIR, "..", "lib"))
-ext_path = File.expand_path(File.join(SPEC_DIR, "..", "ext"))
 $LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
-$LOAD_PATH.unshift ext_path unless $LOAD_PATH.include?(ext_path)
 
 require "rygments"
 require "wrapper"
